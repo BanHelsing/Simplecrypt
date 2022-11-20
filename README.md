700b45ba1b36a3e9a4ff7bd6efdbb9d689ca322b
Ian <mrjoystickerox@gmail.com>
2022-11-19 22:51:03 -0300

    Rebuild
    Changed the selection of characters from a randint function call to a choice from a premade list

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Enigma
 
-Simple encryption module for strings and text files with a homemade algorithm, should not be used for anything serious but will provide enough offuscation to make it unreadable, and it would be really hard to decode by hand
+## Dependencies:
+    - Random
+    - Regex
+
+Simple encryption module for strings and text files coded in base ASCII with a homemade algorithm, should not be used for anything serious but will provide enough offuscation to make it unreadable, and it would be really hard to decode by hand.
--- simplecrypt.py
+++ simplecrypt.py
@@ -1,6 +1,41 @@
 import random as r
+import regex as re
 
-def encrypt(message, key):
+#excluded = [34, 44]
+#        = [ ",  ']
+valid = [i for i in range(33,126)]
+#valid = [i for i in range(33,126) if i not in excluded]
+
+
+def chk_key(key) -> bool:
+    """Checks the validity of the given key
+
+    Args:
+        key (str): The key to check
+
+    Returns:
+        bool: True if valid, False otherwise
+    """
+    return True if re.match("^([a-z]+)\d$", key) else False
+    
+    
+def encrypt(message, key, file_name = "") -> str:
+    """Encrypts based on the key provided      
+
+    Args:
+        message (str): The string to be encrypted
+        key (str): The key used for encryption should be a short word followed
+            by a single number with no whitespace inbetween them.
+            eg: sword5, main8, magnetic2
+
+    Raises:
+        Exception: If the key doesn't follow the format
+
+    Returns:
+        str: Encrypted string
+    """
+    if not chk_key(key):
+        raise Exception("Invalid encryption key")
     counter = 0
     coded_message = ""
     key_num = int(key[-1])
@@ -11,21 +46,44 @@ def encrypt(message, key):
             if letter+key < 32 or letter+key > 125:
                 key = key_num
             coded_message += chr(letter+key)
-            coded_message += chr(r.randint(33,126))
+            coded_message += chr(r.choice(valid))
+            #coded_message += chr(r.randint(33,126))
         else:
             if letter+key < 32 or letter+key > 125:
                 key = key_num
             coded_message += chr(letter-key)
-            coded_message += chr(r.randint(33,126))
+            coded_message += chr(r.choice(valid))
+            #coded_message += chr(r.randint(33,126))
         if counter % 2 == 0:
             key += key_num
         else:
             key -= key
         counter += 1
-    return coded_message
+    if file_name == "":
+        return coded_message
+    with open(file_name,'w', encoding="ascii") as f:
+        f.write(coded_message)
+        f.close()
+        return coded_message
 
 
-def decrypt(message, key):
+def decrypt(message, key) -> str:
+    """Decrypts based on the key provided
+
+    Args:
+        message (str): The string to be decrypted
+        key (str): The key used for decryption should be a short word followed
+            by a single number with no whitespace inbetween them.
+            eg: sword5, main8, magnetic2
+
+    Raises:
+        Exception: If the key doesn't follow the format
+
+    Returns:
+        str: Decrypted string
+    """
+    if not chk_key(key):
+        raise Exception("Invalid encryption key")
     counter = 0
     decoded_message = ""
     key_num = int(key[-1])
